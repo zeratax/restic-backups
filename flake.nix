0a336142746ad3a20606df74031d0a9c59ba9930
ZerataX <mail@zera.tax>
2023-12-01 22:05:32 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A dev shell for Restic with S3 support";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.restic ];
+        };
+      }
+    );
+}
+
